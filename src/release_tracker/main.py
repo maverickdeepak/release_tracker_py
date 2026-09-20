33a72f5b12203820564877fcd2b968b35cff03e2
maverickdeepak <imdeepakchauhan12@gmail.com>
2026-09-20 18:02:55 +0530

    Add create and delete project endpoints; implement slugify utility

--- src/release_tracker/main.py
+++ src/release_tracker/main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI, HTTPException, status, Depends
 from sqlmodel import Session, select
 
 from release_tracker.database import get_session
-from release_tracker.models import Project, ProjectRead
+from release_tracker.models import Project, ProjectRead, ProjectCreate, ProjectUpdate
+from release_tracker.utils import slugify
 
 app = FastAPI(
     title="Release Tracker",
@@ -12,6 +13,7 @@ app = FastAPI(
 )
 
 SessionDep = Annotated[Session, Depends(get_session)]
+
 
 # get a list of all projects, optionally filtered by name
 @app.get("/projects", response_model=list[ProjectRead], status_code=status.HTTP_200_OK)
@@ -50,3 +52,25 @@ def get_project_by_slug(project_slug: str, session: SessionDep) -> Sequence[Proj
             status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
         )
     return projects
+
+# create new project
+@app.post("/projects", response_model=ProjectRead, status_code= status.HTTP_201_CREATED)
+def create_project(payload: ProjectCreate, session: SessionDep) -> Project:
+    project = Project.model_validate(payload, update={
+        "slug": slugify(payload.name)
+    })
+    session.add(project)
+    session.commit()
+    session.refresh(project)
+    return project
+
+# delete a project
+@app.delete("/projects/{project_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_project(project_id: int, session: SessionDep) -> None:
+    project = session.get(Project, project_id)
+    if not project:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
+        )
+    session.delete(project)
+    session.commit()
